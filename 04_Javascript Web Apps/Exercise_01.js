8b7f9852f7144406f268148d0816464211a6887e
Gabriel Frischknecht <gabriel.frischknecht@isolutions.ch>
2018-09-24 10:50:01 +0200

    added a morseCodeToChar function

--- 04_Javascript Web Apps/Exercise_01.js
+++ 04_Javascript Web Apps/Exercise_01.js
@@ -1,18 +1,30 @@
-var alphabetString = "a=.-;b=-...;c=-.-.;d=-..;e=.;f=..-;g=--.;h=....;i=..;j=.---;k=-.-;l=.-..;"+
-		"m=--;n=-.;o=---;p=.---.;q=--.-;r=.-.;s=...;t=-;u=..-;v=...-;w=.--;x=-..-;"+
-    "y=-.-;z=--..; =//;.=.-.-.-;,=--..--;?=..--..";
+var alphabetString = "a=.-;b=-...;c=-.-.;d=-..;e=.;f=..-;g=--.;h=....;i=..;j=.---;k=-.-;l=.-..;" +
+  "m=--;n=-.;o=---;p=.---.;q=--.-;r=.-.;s=...;t=-;u=..-;v=...-;w=.--;x=-..-;" +
+  "y=-.-;z=--..; =//;.=.-.-.-;,=--..--;?=..--..";
+
 var dictionary = {};
 alphabetString.split(";").forEach(value => {
-  dictionary[ value.split("=")[0]] =  value.split("=")[1];
+  dictionary[value.split("=")[0]] = value.split("=")[1];
 });
 
-var charToMorseCode = function(letter){
+var morseCodeToChar = function (morseCode) {
+  return Object.keys(dictionary).find(key => dictionary[key] === morseCode);
+}
+
+var charToMorseCode = function (letter) {
+  if (typeof letter === 'string' && isNaN(letter) && letter.length === 1) {
+    return dictionary[letter];
+  }
+}
+y
+var convertToMorse = function (word) {
   var morseCode = "";
-  letter.split('').forEach(char => {
-    morseCode += ' ' + dictionary[char];
+  word.split('').forEach(char => {
+    morseCode += ' ' + charToMorseCode(char);
   });
   return morseCode;
 }
- 
-console.log(charToMorseCode("acdas"))
+
+console.log(convertToMorse("acdas"))
 console.log(charToMorseCode("b"))
+console.log(morseCodeToChar("..."));
